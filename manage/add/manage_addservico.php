75feba82cc80ad9f8fc8117f956f90d4f0ee0f89
fezamba <fepetitt@gmail.com>
2025-06-23 14:13:55 -0300

    feat : select revisor at criar_ficha POST

--- manage/add/manage_addservico.php
+++ manage/add/manage_addservico.php
@@ -222,6 +222,22 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && $_POST['acao'] === 'criar_servico' 
     $criador
   );
 
+  $revisores_selecionados = $_POST['revisores_ids'] ?? [];
+
+  $stmt_delete = $mysqli->prepare("DELETE FROM servico_revisores WHERE servico_id = ?");
+  $stmt_delete->bind_param("i", $id);
+  $stmt_delete->execute();
+  $stmt_delete->close();
+
+  if (!empty($revisores_selecionados)) {
+    $stmt_assign = $mysqli->prepare("INSERT INTO servico_revisores (servico_id, revisor_id) VALUES (?, ?)");
+    foreach ($revisores_selecionados as $revisor_id) {
+      $stmt_assign->bind_param("ii", $id, $revisor_id);
+      $stmt_assign->execute();
+    }
+    $stmt_assign->close();
+  }
+
   if ($stmt_insert->execute()) {
     $id_servico = $stmt_insert->insert_id;
     $stmt_insert->close();
